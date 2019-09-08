e3c0b8d448fabe43ec0abb526fbfdf5174914712
dransonjs <dranson_ok@126.com>
2019-09-08 23:05:27 +0800

    1

--- PycharmProjects/Python_Automated_Testing_Class_16/practice/verification_code/distinguish.py
+++ PycharmProjects/Python_Automated_Testing_Class_16/practice/verification_code/distinguish.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from PIL import Image
 
 
 browser = Chrome()
@@ -9,6 +10,16 @@ browser.implicitly_wait(10)
 browser.get("https://www.chaojiying.com/user/login/")
 
 wait = WebDriverWait(browser, 10)
-username_e = wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@name='user']")))
-password_e = wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@name='pass']")))
+username = wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@name='user']")))
+password = wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@name='pass']")))
 
+username.send_keys("dranson")
+password.send_keys("348862312")
+
+verify_code_pic = wait.until(EC.visibility_of_element_located((By.XPATH, "//form[@name='fm2']/div/img")))
+code_pic_loc = verify_code_pic.location
+code_pic_size = verify_code_pic.size
+left = code_pic_loc["x"]
+up = code_pic_loc["y"]
+right = left + code_pic_size["width"]
+down = up + code_pic_size["height"]
